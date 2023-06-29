691d504746c0bb43e45a23e49360e7959ec5eec3
Nogui Labs <joannoguera@uoc.edu>
2023-06-29 18:04:32 +0200

    Udemy Course
    Udemy Course first (simple) contract to learn how to interact with the blockchain, deploy a contract and other easy stuff

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+    'hope across ill matrix hotel chest forest stage aware walk sing squeeze',
+    'https://sepolia.infura.io/v3/c96fa91c07d54132bfa7edbaed1900d2'
+
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+ 
+    console.log('Attempting to deploy from account', accounts[0]);
+ 
+    const result = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
